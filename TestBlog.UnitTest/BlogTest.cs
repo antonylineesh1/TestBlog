80c8c8ac2719a703b79e91c904ac251baed37825
antonylineesh1 <antonylineesh1@gmail.com>
2021-01-09 19:28:43 +0530

    unit test

---
+++ TestBlog.UnitTest/BlogTest.cs
@@ -0,0 +1,23 @@
+using System;
+using TestBlog.Controllers;
+using TestBlog.Repository.Entities;
+using Xunit;
+
+namespace TestBlog.UnitTest
+{
+    public class BlogTest
+    {
+        [Fact]
+        public void GetBlog_Returns_NotNull()
+        {
+            // Arrange
+
+
+            // Act
+
+
+            //Assert
+
+        }
+    }
+}
--- TestBlog/Models/CommentDTO.cs
+++ TestBlog/Models/CommentDTO.cs
@@ -1,8 +1,11 @@
-﻿namespace TestBlog.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestBlog.Models
 {
     public class CommentDTO
     {
         public int Id { get; set; }
+        [Required]
         public string Content { get; set; }
     }
 }
